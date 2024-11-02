92470b9cd490a48d0d205ed0cc22dc7e3249e9e6
Damian Betebenner <dbetebenner@nciea.org>
2024-11-02 06:57:52 -0400

    Adding in percentile trajectory calculation functionality

---
+++ R/getPercentileTrajectories.R
@@ -0,0 +1,88 @@
+`getPercentileTrajectories` <- 
+function(
+         ss.data,
+         state,
+         projection.splineMatrices,
+         growth.distribution=NULL
+) {
+
+        ## Utility functions
+        get.growth.distribution.projection.sequence <- function(growth.distribution, years.projected) {
+            if (is.null(growth.distribution)) growth.distribution <- "UNIFORM-RANDOM"
+            growth.distribution <- toupper(growth.distribution)
+            supported_distributions <- c("UNIFORM-RANDOM", as.character(1:99))
+            
+            # Check for unsupported growth distribution & correct length
+            if (!all(growth.distribution %in% supported_distributions)) {
+                stop(paste("Unsupported 'growth.distribution' supplied: Currently supported 'growth.distribution':", 
+                        paste(sapply(supported_distributions, SGP::capwords), collapse = ", ")))
+            }
+            
+            if (!length(growth.distribution) %in% c(1, years.projected)) {
+                stop(paste0("Length of supplied growth distribution must be either 1 or ", years.projected, "."))
+            }
+            
+            # Return values based on the input growth.distribution
+            if (growth.distribution == "UNIFORM-RANDOM") return(rep("UNIFORM-RANDOM", years.projected))
+            if (length(growth.distribution) == 1L && growth.distribution %in% as.character(1:99)) return(rep(growth.distribution, years.projected))
+            return(rep(growth.distribution, years.projected))
+        }
+
+        get.boundaries <- function(state, content_area, grade) {
+            return(SGP::SGPstateData[[state]][['Achievement']][['Knots_Boundaries']][[content_area]][[paste("boundaries", grade, sep="_")]])
+        }
+
+        get.subset.indices <- function(ss.data, growth.distribution) {
+
+            if (growth.distribution=="UNIFORM-RANDOM") {
+                tmp.quantiles <- runif(dim(ss.data)[1L], min = 0, max = 100)
+                my.indices <- findInterval(tmp.quantiles, seq(0.5, by = 1, length = 100), rightmost.closed = TRUE)
+                my.indices <- pmin(pmax(my.indices, 1L), 99L)
+                return(my.indices)
+            }
+
+            if (growth.distribution %in% as.character(1:99)) {
+                return(rep(as.integer(growth.distribution), dim(ss.data)[1L]))
+            }
+        }
+
+        bound.iso.subset.scores <- function(projected.scores, boundaries, subset.indices) {
+            ## Pull in outlier to loss/hoss
+            projected.scores[TEMP_1 < boundaries[1L], TEMP_1:=boundaries[1L]]
+            projected.scores[TEMP_1 > boundaries[2L], TEMP_1:=boundaries[2L]]
+
+            ## isotonize projections 
+            setkey(projected.scores, ID, TEMP_1)
+            subset.indices <- c(rbind((subset.indices + (seq_along(subset.indices) - 1L) * 100L), (1L + subset.indices + (seq_along(subset.indices) - 1L) * 100L)))
+            return(projected.scores[subset.indices, list(TEMP_2=mean(TEMP_1)), keyby="ID"][['TEMP_2']])
+		}
+
+        ## Parameters
+        growth.distribution.projection.sequence <- get.growth.distribution.projection.sequence(growth.distribution, length(projection.splineMatrices[[1]]))
+
+        ## Loop over daisy-chained, matrix sequence
+        for (i in seq_along(projection.splineMatrices)) {
+            label.iter <- 1L
+            for (j in seq_along(projection.splineMatrices[[i]])) {
+		    	tmp.matrix <- projection.splineMatrices[[i]][[j]]
+                boundaries <- get.boundaries(state, tail(tmp.matrix@Content_Areas[[1]], 1), tail(tmp.matrix@Grade_Progression[[1]], 1))
+                subset.indices <- get.subset.indices(ss.data, growth.distribution.projection.sequence[j])
+
+		    	mod <- character()
+		    	int <- "data.table(ID=ss.data[[1L]], INT=1L,"
+		    	for (model.iter in seq_along(projection.splineMatrices[[i]][[j]]@Time_Lags[[1L]])) {
+		    		knt <- paste0("tmp.matrix@Knots[[", model.iter, "]]")
+			    	bnd <- paste0("tmp.matrix@Boundaries[[", model.iter, "]]")
+			    	mod <- paste0(mod, ", bs(ss.data[[", dim(ss.data)[2L]-model.iter+1L, "]], knots=", knt, ", Boundary.knots=", bnd, ")")
+		    	}
+
+    			tmp.scores <- eval(parse(text=paste0(int, substring(mod, 2L), ", key='ID')")))
+                projected.scores <- melt(as.data.table(as.matrix(tmp.scores[,-1]) %*% tmp.matrix@.Data)[,ID:=tmp.scores[['ID']]], id.vars="ID", value.name="TEMP_1")[,variable:=NULL]
+                ss.data[,TEMP_2:=bound.iso.subset.scores(projected.scores, boundaries, subset.indices)]
+    			setnames(ss.data, "TEMP_2", paste0("SS", tail(tmp.matrix@Grade_Progression[[1]], 1)))
+	    		label.iter <- label.iter + 1L
+	    	} ## END j loop
+        } ## END i loop
+
+        return(ss.data)
+} ### END getPercentileTrajectories
--- R/zzz.R
+++ R/zzz.R
@@ -10,12 +10,11 @@
   if (interactive()) {
     # Extract version information
     version <- packageVersion("sgpFlow")
-    formatted_version <- gsub("\\.", "-", as.character(version))  # Replace dots with dashes
     
     # Define a friendly startup message
     message_text <- paste0(
-      magenta$bold("sgpFlow"), " v", formatted_version, 
-      " (10-29-2024)\n",
+      magenta$bold("sgpFlow"), " v", version, 
+      " (11-2-2024)\n",
       "For help, type: ", magenta$bold("> help(\"sgpFlow\")"), " or visit:\n",
       magenta$bold("https://centerforassessment.github.io/sgpFlow")
     )
